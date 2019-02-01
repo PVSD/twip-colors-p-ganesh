227a41debd8a2a6676d60d63304869c49ff6165e
p-ganesh <pg304@pvsd.org>
2019-02-01 10:48:57 -0500

    dsfg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kb = new Scanner (System.in);
+        System.out.println("Do you want to convert RGB, Hexadecimal, or HTML?");
+        String s = kb.next();
+        System.out.println("Enter what you would like to convert:");
+        String k = kb.next();
+
+        if (s.equalsIgnoreCase("RGB")) {
+
+        }
+        else if (s.equalsIgnoreCase("Hexadecimal")) {
+
+        }
+        else if (s.equalsIgnoreCase("HTML")) {
+
+        }
+
+
     }
 }
---
+++ src/com/company/convert.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by pg304 on 1/29/19.
+ */
+public class convert {
+    private int decimal [] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    private String hex [] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+
+    public String toHex (int s){
+
+    }
+}
